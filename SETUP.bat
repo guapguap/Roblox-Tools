1765878ad09d5089bc76ce3f6fc76b51dcdd9f9c
zjya <145272184+guapguap@users.noreply.github.com>
2025-07-05 22:10:49 +0200

    Create SETUP.bat

---
+++ SETUP.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+where python >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    pause
+    exit /b 1
+)
+
+where pip >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    python -m ensurepip --upgrade
+    python -m pip install --upgrade pip
+    if %ERRORLEVEL% neq 0 (
+        pause
+        exit /b 1
+    )
+)
+
+python -m pip install --upgrade pip
+if %ERRORLEVEL% neq 0 (
+    pause
+    exit /b 1
+)
+
+pip install requests customtkinter pyinstaller pywin32 python-dotenv
+if %ERRORLEVEL% neq 0 (
+    pause
+    exit /b 1
+)
+
+for %%i in (requests customtkinter pyinstaller pywin32 python_dotenv) do (
+    pip show %%i >nul 2>&1
+    if !ERRORLEVEL! neq 0 (
+        pause
+        exit /b 1
+    )
+)
+
+pause
+exit /b 0
